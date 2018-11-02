54b410f99e85ddb8491dcee0a357bfdf00853f5d
karolissto <ltkarolissto@gmail.com>
2018-11-02 21:24:46 +0200

    Tado patvarkiau

--- Gear/Controllers/GameController.cs
+++ Gear/Controllers/GameController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Gear.Models;
 
 
 /// <summary>
@@ -15,7 +16,13 @@ namespace Gear.Controllers
         // GET: GamePage
         public ActionResult Index()
         {
-            return View();
+            var game = new Game()
+            {
+                Name = "The Witcher® 3: Wild Hunt",
+                Description = "Experience the epic conclusion to the story of professional monster slayer, witcher Geralt of Rivia. As war rages on throughout the Northern Realms, you take on the greatest contract of your life — tracking down the Child of Prophecy, a living weapon that can alter the shape of the world."
+            };
+            return View(game);
         }
+
     }
 }
