4169a2c3c76d674d47973b4577ec4526f999f76e
karolissto <ltkarolissto@gmail.com>
2018-11-05 01:49:17 +0200

    Added games list

--- Gear/Controllers/ProfileController.cs
+++ Gear/Controllers/ProfileController.cs
@@ -16,6 +16,14 @@ namespace Gear.Controllers
         // GET: Profile
         public ActionResult Index()
         {
+            UserGame UG = new UserGame();
+            UG.user = GetUser();
+            UG.games = GetGames();
+            return View(UG);
+        }
+
+        public User GetUser()
+        {
             var profile = new User()
             {
                 Username = "Karolissto",
@@ -24,7 +32,27 @@ namespace Gear.Controllers
                 Surname = "Stončius",
                 Avatar = "/Content/images/avatar.jpg"
             };
-            return View(profile);
+            return profile;
+        }
+        public List<Game> GetGames()
+        {
+            List<Game> games = new List<Game>();
+            games.Add(new Game() { Name = "Civilization V", Id = 1, Description = "2018-10-05", Icon = "25"});
+            games.Add(new Game() { Name = "Arma 3", Id = 2, Description = "2018-11-01", Icon = "105" });
+            games.Add(new Game() { Name = "Team Fortress", Id = 3, Description = "2018-05-15", Icon = "54" });
+            games.Add(new Game() { Name = "Warframe", Id = 4, Description = "2018-10-03", Icon = "12" });
+            games.Add(new Game() { Name = "CSGO", Id = 5, Description = "2018-09-24", Icon = "2" });
+            return games;
+        }
+
+        public class ViewModel
+        {
+            public IEnumerable<User> Users { get; set; }
+            public IEnumerable<Game> Games { get; set; }
         }
     }
+
+   
+
+    
 }
