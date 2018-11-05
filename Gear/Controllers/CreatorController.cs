0816d21675c54e4501bad60e5defcbe79ba06f6f
Gabrielius Ulejevas <ule2ga@yahoo.com>
2018-11-05 15:54:50 +0200

    Same more done

--- Gear/Controllers/CreatorController.cs
+++ Gear/Controllers/CreatorController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Gear.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -20,6 +22,22 @@ namespace Gear.Controllers
         public ActionResult Add()
         {
             return View(@"~/Views/Creator/Edit.cshtml");
+        }
+        public ActionResult Statistics()
+        {
+            double count = 1000, y = 100;
+            Random random = new Random();
+            List<DataPoint> dataPoints = new List<DataPoint>();
+
+            for (int i = 0; i < count; i++)
+            {
+                y += random.Next(-10, 11);
+                dataPoints.Add(new DataPoint(i, y));
+            }
+
+            ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
+
+            return View(@"~/Views/Creator/Statistics.cshtml");
         }
     }
 }
