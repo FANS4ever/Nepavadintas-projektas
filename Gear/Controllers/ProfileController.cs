4a16c2e77594a658f0e349c0ba763219b4c82eae
FANS4ever <domciukas333@gmail.com>
2018-12-08 14:26:43 +0200

    Register form v0.1

--- Gear/Controllers/ProfileController.cs
+++ Gear/Controllers/ProfileController.cs
@@ -13,93 +13,52 @@ namespace Gear.Controllers
 {
     public class ProfileController : Controller
     {
-        //IList<User> userList = new List<User>()
-        //{
-        //    new User(){Username = "Karolissto",
-        //        Email = "ltkarolissto@gmail.com",
-        //        Name = "Karolis",
-        //        Surname = "Stončius",
-        //        Avatar = "/Content/images/avatar.jpg"}
-        //};
-        //// GET: Profile
-        //public ActionResult Index()
-        //{
-        //    UserGame UG = new UserGame();
-        //    UG.user = GetUser();
-        //    UG.games = GetGames();
-        //    return View(UG);
-        //}
+        GearDBEntities db = new GearDBEntities();
 
-        //public ActionResult Edit()
-        //{
-        //    UserGame UG = new UserGame();
-        //    UG.user = GetUser();
-        //    UG.games = GetGames();
-        //    UG.country = new Country() { };          
-        //    return View(UG);
-        //}
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
 
-        //public ActionResult ChatRoom()
-        //{
-        //    Message M = new Message();
-        //    M.messages = GetMessage();
-        //    return View(M);
-        //}
 
-        //[HttpPost]
-        //public ActionResult Edit(UserGame UG)
-        //{
-        //    return RedirectToAction("Index");
-        //}
+        public ActionResult Edit()
+        {
+            return View();
+        }
 
-        //public List<Message> GetMessage()
-        //{
-        //    List<Message> messages = new List<Message>();
-        //    messages.Add(new Message() { Id = 1, Contents = "Laba diena", SendDate = DateTime.Now });
-        //    messages.Add(new Message() { Id = 2, Contents = "Kaip sekasi?", SendDate = DateTime.Now });
-        //    messages.Add(new Message() { Id = 3, Contents = "Ka veiki?", SendDate = DateTime.Now });
-        //    messages.Add(new Message() { Id = 4, Contents = "Sumetam cs matcha?", SendDate = DateTime.Now });
-        //    messages.Add(new Message() { Id = 5, Contents = "Noobas", SendDate = DateTime.Now });
-        //    return messages;
-        //}
+        public ActionResult ChatRoom()
+        {
+            return View();
+        }
 
-        //public User GetUser()
-        //{
-        //    var profile = new User()
-        //    {
-        //        Username = "Karolissto",
-        //        Email = "ltkarolissto@gmail.com",
-        //        Name = "Karolis",
-        //        Surname = "Stončius",
-                //Avatar = "/Content/images/Avatars/avatar.jpg"
-        //    };
-        //    return profile;
-        //}
-        //public List<Game> GetGames()
-        //{
-        //    List<Game> games = new List<Game>();
-            //games.Add(new Game() { Name = "Civilization V", Id = 1, Description = "Turn-Based Strategy", ReleaseTime = new DateTime(2010,09,23), Icon = "/Content/images/GameIcons/Civ5.jpg" });
-            //games.Add(new Game() { Name = "Arma 3", Id = 2, Description = "Simulation, FPS", ReleaseTime = new DateTime(2013, 09, 12), Icon = "/Content/images/GameIcons/Arma3.jpg" });
-            //games.Add(new Game() { Name = "Team Fortress 2", Id = 3, Description = "Multiplayer, FPS", ReleaseTime = new DateTime(2007, 10, 10), Icon = "/Content/images/GameIcons/TeamFortress2.jpg" });
-            //games.Add(new Game() { Name = "Warframe", Id = 4, Description = "Action", ReleaseTime = new DateTime(2013, 03, 25), Icon = "/Content/images/GameIcons/Warframe.jpg" });
-            //games.Add(new Game() { Name = "Counter-strike: Global Offensive", Id = 5, Description = "FPS", ReleaseTime = new DateTime(2012, 08, 21), Icon = "/Content/images/GameIcons/CSGO.jpg" });
-        //    return games;
-        //}
+        [HttpGet]
+        public ActionResult Register()
+        {
+            List<Country> model = db.Countries.ToList();
+            return View(model);
+        }
 
-        //public class ViewModel
-        //{
-        //    public IEnumerable<User> Users { get; set; }
-        //    public IEnumerable<Game> Games { get; set; }
-        //}
+       [HttpPost]
+        public ActionResult Register(string username, string email, string password, string confirmPassword, int country)
+        {
+            db.Users.Add(new User()
+            {
+                Username = username,
+                Password = password,
+                Email = email,
+                Blocked = 0,
+                Country_Code = country,
+                Rank_Name = "User"
+            });
+            db.SaveChanges();
 
-        //public ActionResult Register()
-        //{
-        //    return View();
-        //}
+            return Content("Good");
+        }
 
-        //public ActionResult Login()
-        //{
-        //    return View();
-        //}
+        public ActionResult Login()
+        {
+            return View();
+        }
     }
 }
