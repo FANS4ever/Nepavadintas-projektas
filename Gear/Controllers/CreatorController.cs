e117e3906ec1549114758fbed917a4e18b781283
Griebia <ule2ga@yahoo.com>
2018-12-11 13:30:49 +0200

    Mmmm

--- Gear/Controllers/CreatorController.cs
+++ Gear/Controllers/CreatorController.cs
@@ -14,12 +14,30 @@ namespace Gear.Controllers
 {
     public class CreatorController : ControllerBase
     {
-        
+
         // GET: CreatorPage
         public ActionResult Index()
         {
-            return View();
+            Developer developer = null;
+            List<Game> games = new List<Game>();
+            string loggedUserName = (string)Session["Username"];
+            if ((bool)Session["LoggedIn"])
+            {
+               developer = db.Developers.Where(x => x.Users.Any(y => y.Username.Contains(loggedUserName))).ToList()[0];
+               games = db.Games.Where(x => x.Developer_Id == developer.Id).ToList();
+            }
+             
+
+            return View(games);
         }
+        public ActionResult Remove(int id) {
+
+            Game game = db.Games.Where(x => x.Id == id).ToList()[0];
+            db.Games.Remove(game);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
         public ActionResult Edit()
         {
             return View(@"~/Views/Creator/Edit.cshtml");
@@ -46,11 +64,31 @@ namespace Gear.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(string gamename, string message, HttpPostedFileBase pic, string trailer, string version,string genre, string age, string date, HttpPostedFileBase file, string price)
+        public ActionResult Create(string gamename, string message, string pic, string trailer, string version,string genre, string age, DateTime date, HttpPostedFileBase file, string price)
         {
+            string loggedUserName = (string)Session["Username"];
+            Developer developer = db.Developers.Where(x => x.Users.Any(y => y.Username.Contains(loggedUserName))).ToList()[0];
 
-            return View(@"~/Views/Creator/Create.cshtml");
-        }
+            Game addGame = new Game()
+            {
+                Name = gamename,
+                Description = message,
+                ShortDescription = "ner",
+                TrailerURL = trailer,
+                Price = double.Parse(price),
+                VersionBranch = version,
+                VersionNumber = 1.0,
+                Dir = pic,
+                Available = (sbyte)1,
+                ReleaseDate = date,
+                AgeRestriction = int.Parse(age)
+                
+            };
+            addGame.Developer_Id = developer.Id;
+            db.Games.Add(addGame);
+            db.SaveChanges();
+            return View();
+    }
         public ActionResult BecomingCreator()
         {
             return View(@"~/Views/Creator/BecomingCreator.cshtml");
@@ -60,8 +98,8 @@ namespace Gear.Controllers
         public ActionResult BecomingCreator(string name, string website)
         {
             //Changes users status to Creator
-            var user = Session["User"] as User;
-            User userInfo = db.Users.Where(s=>s.Username == user.Username).FirstOrDefault();
+            var username = (string)Session["Username"];
+            User userInfo = db.Users.Where(s=>s.Username == username).FirstOrDefault();
             if (userInfo != null)
             {
                 userInfo.Rank_Name = "Creator";
@@ -76,7 +114,7 @@ namespace Gear.Controllers
 
             db.SaveChanges();
             var connection = db.Developers.Where(x => x.Users.Any(r => userInfo.Username.Contains(r.Username)));
-            return Content(connection.ToList()[0].Id.ToString());
+            return RedirectToAction("Index");
         }
     }
 }
